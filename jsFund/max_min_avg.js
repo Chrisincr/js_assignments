cb7fdb8df991bcdbb6e914533f5c3daa834770a8
Your Name <you@example.com>
2019-03-28 11:53:33 -0500

    fizzbuzz commit

--- jsFund/max_min_avg.js
+++ jsFund/max_min_avg.js
@@ -15,4 +15,48 @@ function minMaxAvg(arr){
 //console.log(minMaxAvg([1,-2,9,4]))
 
 
+function fizzbuzz(n){
+    if (n < 1 || typeof n != 'number'){
+        console.log('Parameter must be a positive number')
+        return
+    }
 
+
+    let i =1
+    let z = 0
+    function check(x){
+        z = 0
+        if(x%3 == 0)
+            z = 1
+        if(x%5 == 0)
+            z = 2
+        if(x%15 == 0)
+            z = 3
+
+        return z
+    }
+
+    while (i <= n){
+        let expr = check(i)
+        switch(expr){
+            case 3:
+            console.log('FizzBuzz')
+            break;
+
+            case 1:
+            console.log('Fizz')
+            
+            break;
+
+            case 2:
+            console.log('Buzz')
+            
+            break;
+
+            default:
+            
+            console.log(i)
+        }
+        i++
+    }
+}
